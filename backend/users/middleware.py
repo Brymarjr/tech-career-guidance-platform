46c979fa8e79c916892ae5fbdb77fa893c82822d
Brymarjr <braimaholatilewa@gmail.com>
2026-03-21 01:00:25 +0100

    Fixed the async and sync issues of the app not being able to handle both

--- backend/users/middleware.py
+++ backend/users/middleware.py
@@ -1,31 +1,27 @@
 from django.utils import timezone
 from .models import CustomUser
-from asgiref.sync import sync_to_async
-from django.utils.deprecation import MiddlewareMixin
+from asgiref.sync import sync_to_async, iscoroutinefunction
+from django.utils.decorators import sync_and_async_middleware
 
-class UpdateLastActivityMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-        # Check if the get_response is a coroutine (Async)
-        import asyncio
-        self._is_coroutine = asyncio.iscoroutinefunction(get_response)
-
-    async def __call__(self, request):
-        # Handle the Async call (Daphne/Production)
-        if self._is_coroutine:
-            response = await self.get_response(request)
+@sync_and_async_middleware
+def UpdateLastActivityMiddleware(get_response):
+    # This logic works for both Sync (runserver) and Async (Daphne)
+    if iscoroutinefunction(get_response):
+        async def middleware(request):
+            response = await get_response(request)
             if request.user.is_authenticated:
-                await self.update_activity(request.user.id)
+                # Update DB in background safely
+                await sync_to_async(update_user_activity)(request.user.id)
             return response
-        
-        # Handle the Sync call (Local/Development)
-        response = self.get_response(request)
-        if request.user.is_authenticated:
-            # We call the same logic but don't need to await here
-            CustomUser.objects.filter(id=request.user.id).update(last_activity=timezone.now())
-        return response
+    else:
+        def middleware(request):
+            response = get_response(request)
+            if request.user.is_authenticated:
+                update_user_activity(request.user.id)
+            return response
 
-    @sync_to_async
-    def update_activity(self, user_id):
-        # This runs safely in a thread-pool so it won't block Daphne
-        CustomUser.objects.filter(id=user_id).update(last_activity=timezone.now())
+    return middleware
+
+def update_user_activity(user_id):
+    """Simple sync function to update the timestamp"""
+    CustomUser.objects.filter(id=user_id).update(last_activity=timezone.now())
